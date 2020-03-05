14913bc8101609dc4f1fb51b2746f868454d690d
Nathaniel7144 <41036605+Nathaniel7144@users.noreply.github.com>
2020-03-06 01:27:51 +0700

    PipeSpawn random

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    //Global Variable
+    [SerializeField] private Bird bird;
+    [SerializeField] private float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Cek burung sudah mati/ belum
+        if(!bird.IsDead())
+        {
+            //Membuat pipa bergerak ke sebelah kiri dengan kecepatan tertentu
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+
+        //cek null value
+        if(bird)
+        {
+            //dapatkan component collider pada game object
+            Collider2D collider = GetComponent<Collider2D>();
+
+            //cek null variable atau tidak
+            if(collider)
+            {
+                //non aktifkan cllider
+                collider.enabled = false;
+            }
+
+            bird.Dead();
+        }
+    }
+}
---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    //Global Variables
+    [SerializeField] private Bird bird;
+    [SerializeField] private Pipe pipeUp, pipeDown;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private float holeSize = 1f;
+
+    //Variable penampung coroutine
+    private Coroutine CR_Spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawn
+        StartSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartSpawn()
+    {
+        //Menjalankan fungsi Coroutine IeSpawn
+        if(CR_Spawn == null)
+        {
+            CR_Spawn = StartCoroutine(IeSpawn());
+        }
+    }
+
+    void StopSpawn()
+    {
+        //Hentikan coroutine
+        if(CR_Spawn != null)
+        {
+            StopCoroutine(CR_Spawn);
+        }
+    }
+
+    void SpawnPipe()
+    {
+        //duplikasi game object pipeUp dan menempatkan posisinya sama 
+        //dengan game object ini tapi dibalik
+        Pipe newPipeUp = Instantiate(pipeUp, transform.position, Quaternion.Euler(0,0,180));
+
+        //aktifkan newPipeUp
+        newPipeUp.gameObject.SetActive(true);
+
+        //duplikasi game object pipeDown dan menempatkan posisinya sama
+        //dengan posisi game object
+        Pipe newPipeDown = Instantiate(pipeDown, transform.position, Quaternion.identity);
+
+        //aktifkan newPipeDown
+        newPipeDown.gameObject.SetActive(true);
+
+        //tempatkan posisi pipa yang sudah terbentuk agar ada lubang di tengahnya
+        newPipeUp.transform.position += Vector3.up * (holeSize/2);
+        newPipeDown.transform.position += Vector3.down * (holeSize/2);
+
+        //tempatkan posisi pipa yang telah dibentuk agar posisinya random
+        float y = Random.Range(-1.0f, 1.1f);
+
+        newPipeUp.transform.position += Vector3.up * y;
+        newPipeDown.transform.position += Vector3.up * y;
+        
+    }
+
+    IEnumerator IeSpawn()
+    {
+        while(true)
+        {
+            //Jika burung mati stop
+            if(bird.IsDead())
+            {
+                StopSpawn();
+            }
+
+            //Buat pipa baru
+            SpawnPipe();
+
+            //Tunggu beberapa detik sesuai dengan interval
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
